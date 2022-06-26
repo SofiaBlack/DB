76b71ab314ce636ea957f1ef366248a448f5cfdc
Sofia Montagna <montagnasofia@hotmail.com>
2022-06-26 18:23:20 +0200

    add function queryupdate

--- class/db.php
+++ class/db.php
@@ -13,7 +13,7 @@ public function __construct() {
     $this->DB_NAME = DB_NAME;
     $this->DB_USER = DB_USER;
     $this->DB_PASSWORD = DB_PASSWORD;
-    
+
     try {
         $this->CONN = new PDO("mysql:host=" . $this->DB_HOST . ";dbname=" . $this->DB_NAME, $this->DB_USER, $this->DB_PASSWORD);
         $this->CONN->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -23,13 +23,38 @@ public function __construct() {
     }
 }
 
-public function query($string, $DEBUG=0){
+public function query($string, $DEBUG = 0){
    $this->QUERY = $this->CONN->prepare($string);
    $this->QUERY->execute();
 
    if ($DEBUG == 1) {
        print $string;
     }
+}
+
+public function queryarray() {
+    
+}
+
+/**
+ * $table -> table's name
+ * $values[array] as field -> new value
+ * $where[array|string]
+ * $DEBUG[0|1] if 1 print the query - default = 0
+ */
+public function queryupdate($table, $values, $where, $DEBUG = 0){
+    $sets = array();
+    foreach($values as $data => $value) {
+        $sets[] = $data." = ".$value;
+    }
+    $query = "UPDATE ".$table;
+    $query .= " SET ".implode(', ', $sets);
+    if(is_array($where)) {
+        $query .= " WHERE ".implode(' AND ', $where);
+    } else {
+        $query .= " WHERE ".$where;
+    }
+    $this->query($query, $DEBUG);
 }
 
 public function next_assoc(){
@@ -39,6 +64,8 @@ public function next_assoc(){
 public function num_rec(){
     print $this->QUERY->rowCount();
 }
+
+
 }
 ?>
 
--- index.php
+++ index.php
@@ -5,7 +5,18 @@ $db = new DB();
 
 $db->query("SELECT * FROM login", 0);
 
-$db->num_rec();
+$data['nome'] = 'sofia';
+$data['cognome'] = 'montagna';
+
+$where[] = 'id = 1 AND lingua = ita';
+
+
+$prova = implode(' AND ', $where);
+
+//print_r($where['where']);
+//print($prova);
+
+$db -> queryupdate('prova', $data, $where);
 
 while ($res = $db->next_assoc()){
     print '<pre>';
