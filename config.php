3360ff03102a3ab1864ad9d2164b9ca2a7120b87
Sofia Montagna <montagnasofia@hotmail.com>
2022-11-27 12:41:50 +0100

    settings

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-define("DB_HOST", "localhost" );
-define("DB_NAME", "lihos_minerals_2");
-define("DB_USER", "root");
+define("DB_HOST", "" );
+define("DB_NAME", "");
+define("DB_USER", "");
 define("DB_PASSWORD", "");
 
 require(__DIR__.'\class\db.php');
--- index.php
+++ index.php
@@ -1,18 +1,2 @@
 <?php
 require(__DIR__.'\config.php');
-
-$values = array('username' => "prova 22");
-
-$db = new DB();
-
-$db->queryinsert('login', $values, 1);
-
-print '<br>';
-$db->last_insert_id();
-
-$db->query("SELECT * FROM login", 1);
-while ($res = $db->next_assoc()){
-    print '<pre>';
-    print_r($res);
-    print '</pre>';
-}
